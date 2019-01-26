f9fef2de340b4e2062f0b2ad53ba1e34f952bc1d
mwPlouffe <mathew.plouffe@mail.mcgill.ca>
2019-01-26 12:01:44 -0500

    Changes to be committed: 	new file:   DiceRoller.py 	modified:   Die.py
    Added a Die class and a DieRoller class for rolling dice

---
+++ Die.py
@@ -0,0 +1,11 @@
+import random
+
+class Die:
+	roll_value = 0
+	sides = 0
+	def __init__(self,sides = 6):
+		self.sides = sides
+	def roll(self):
+		roll_value = random.randint(1,self.sides)
+		return roll_value 
+
