cfdbb66943b840ac2df21c5e94f9df3f4f1e640e
mwPlouffe <mathew.plouffe@mail.mcgill.ca>
2019-01-26 13:18:29 -0500

    Changes to be committed: 	modified:   Tile.py
    Updated Tile constructor to provide usable interface for the Board class

---
+++ Tile.py
@@ -0,0 +1,8 @@
+class Tile:
+
+	def __init__(self, resource, activation_value, corners = [], edges = []):
+		self.resource 	      = resource
+		self.activation_value = activation_value
+		self.corners 	      = corners
+		self.edges	      = edges
+		
