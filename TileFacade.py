250b37838ee237d1f89962b7c306441cc1542e3b
mwPlouffe <mathew.plouffe@mail.mcgill.ca>
2019-01-26 21:11:01 -0500

    TileFacades now display the value on which they yield resources  Changes to be committed: 	modified:   TileFacade.py

--- TileFacade.py
+++ TileFacade.py
@@ -9,14 +9,18 @@ class TileFacade:
 	YELLOW = (255, 255, 0)
 	WHITE = (255, 255, 255)
 	def __init__(self, tile, screen, centre = [0, 0], scale = 1):
-		self.tile   = tile
-		self.screen = screen
-		self.points = self.__hex_pointlist_generator(scale - 3, centre)
+		self.tile   	   = tile
+		self.screen 	   = screen
+		self.points 	   = self.__hex_pointlist_generator(scale - 3, centre)
 		self.border_points = self.__hex_pointlist_generator(scale, centre)
-		self.colour = self.__set_colour(self.tile.resource)
+		self.colour 	   = self.__set_colour(self.tile.resource)
+		self.text 	   = self.__set_activation_value( str(self.tile.activation_value)) 
+		self.centre	   = [round(centre[0]), round(centre[1])]
 	def draw(self):
 		pygame.draw.polygon(self.screen, (0, 0, 0), self.border_points, 0)
-		pygame.draw.polygon(self.screen, self.colour, self.points, 0)	
+		pygame.draw.polygon(self.screen, self.colour, self.points, 0)
+		pygame.draw.circle(self.screen, (228, 205, 180), self.centre, 20, 0)
+		self.screen.blit(self.text,[self.centre[0] - 11, self.centre[1] - 8] )
 	
 	def __hex_pointlist_generator(self, scale, centre):
 		ret = []
@@ -24,7 +28,11 @@ class TileFacade:
 			deg = 60.0 * i - 30.0
 			rad = math.pi / 180.0 * deg
 			ret.append([centre[0] + scale * math.cos(rad), centre[1] + scale * math.sin(rad)])
-		return ret	
+		return ret
+	def __set_activation_value(self, activation_value):
+		font = pygame.font.Font(None, 36)
+		return font.render(activation_value, 1, (10,10,10))
+			
 	def __set_colour(self,resource):
 		if resource == "lumber":
 			return TileFacade.GREEN
